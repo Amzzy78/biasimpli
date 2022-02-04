c0ec982ffd1127ca2eadebdd38f0df0d2f978ffc
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-04 18:09:40 +0000

    Add TextField and ManyToManyField to class Post in models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -10,15 +11,18 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
 class Meta:
     ordering = ('-publish',)
@@ -26,3 +30,6 @@ class Meta:
 
 def __str__(self):
     return self.title
+
+def number_of_likes(self):
+    return self.likes.count()    
