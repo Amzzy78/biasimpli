215809248522eac94f998fd979671198d216ccbc
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-09 19:31:13 +0000

    Build model for comments

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+#from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -18,9 +18,12 @@ class Post(models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     #featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
 
 class Meta:
     ordering = ('-publish',)
@@ -28,3 +31,22 @@ class Meta:
 
 def __str__(self):
     return self.title
+
+
+def number_of_likes(self):
+    return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
