1f48ed8f865110afe84a3ad80c320183ca697010
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-10 15:06:18 +0000

    Add html files

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Post
 
 # Create your views here.
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish_year=year, publish_month=month, publish_day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+# def post_detail(request, year, month, day, post):
+#     post = get_object_or_404(Post, slug=post, status='published', publish_year=year, publish_month=month, publish_day=day)
+#     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status='published').order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
