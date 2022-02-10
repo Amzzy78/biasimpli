aed7a4ccc10ecc083b146b998b582d2f7c74c581
Amzzy78 <murphy.annmarie78@gmail.com>
2022-02-10 16:03:04 +0000

    Add custom and default manager models

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -23,7 +29,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
-
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Our custom manager
 
 class Meta:
     ordering = ('-publish',)
@@ -50,3 +57,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
